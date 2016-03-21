7ca8c59db52e61e3f5ae04f4d6ddcb8be477eb83
Sam Ternent <sam.ternent@gmail.com>
2016-03-21 16:20:37 +0000

    adds loaders

--- README.md
+++ README.md
@@ -16,15 +16,36 @@ module.exports = {
   entry: "src/index.js",
   plugins: [
     new webpackLinkPlugin({
-      localModule: path.resolve(__dirname, '../localModule')
+      localModule: path.resolve(__dirname, '../localModule/src')
     })
   ],
   output: {
     filename: "build.js",
-  },
-  resolve: {
-    extensions: [".js"],
   }
 }
+```
 
+### Using loaders
+
+```
+var path = require('path'),
+    webpackLinkPlugin = require('webpack-link')
+
+module.exports = {
+  entry: "src/index.js",
+  plugins: [
+    new webpackLinkPlugin({
+      localModule: [
+        path.resolve(__dirname, '../localeModule/src'),
+        {
+          test: /\.js$/,
+          loader: "babel-loader?presets[]=es2015"
+        }
+      ]
+    })
+  ],
+  output: {
+    filename: "build.js",
+  }
+}
 ```
--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+var assign = require('object-assign');
+
+var getCLIOptions = function () {
+  var modules = [];
+  process.argv.forEach(function (val, index, array) {
+    if (val.indexOf('--link') > -1) {
+      var args = val.split('=')[1];
+      if (args) { modules = args.split(',') }
+    }
+  });
+  return modules;
+};
+
+var formatSettings = function (option) {
+  if (option.constructor !== Array) return { path: option };
+  return {
+    path: option[0],
+    loader: assign(
+      option[1],
+      { include: [option[0]] }
+    )
+  }
+}
+
 var outputStatus = function (module, path) {
   console.log(
     '\x1b[0;33m', 'Linked', module, 'to local path', path, '\x1b[0m'
@@ -6,16 +30,26 @@ var outputStatus = function (module, path) {
 
 function WebpackLinkPlugin(options) {
   this.options = options;
+  this.modules = getCLIOptions();
 }
 
 WebpackLinkPlugin.prototype.apply = function(compiler) {
   var options = this.options;
-  if (!options) { return }
+  var modules = this.modules;
+  if (!modules || !options) { return }
 
   for (var key in options) {
     if (!options.hasOwnProperty(key)) continue;
-    compiler.options.resolve.alias[key] = options[key];
-    outputStatus(key, options[key])
+    if (key.indexOf(modules) > -1) continue;
+
+    var settings = formatSettings(options[key]);
+    compiler.options.resolve.alias[key] = settings.path;
+    outputStatus(key, settings.path);
+
+    if (!settings.loader) continue;
+
+    compiler.options.module.loaders = compiler.options.module.loaders || []
+    compiler.options.module.loaders.push(settings.loader);
   }
 };
 
